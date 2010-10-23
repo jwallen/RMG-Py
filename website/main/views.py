779db4ece6ab4e020c7be54badc74a2bb7fffb05
Josh Allen <jwallen@mit.edu>
2010-10-23 18:16:06 -0400

    Added view for RMG website homepage to main app.
    Now when you run the development server and point your browser at
    http://127.0.0.1:8000/ you will see a real page!

--- website/main/views.py
+++ website/main/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+
+def index(request):
+    """
+    The RMG website homepage.
+    """
+    return render_to_response('index.html', {})
