74834b15e2ea741ab678764398995eda73ec20f1
Josh Allen <jwallen@mit.edu>
2010-10-28 12:20:04 -0400

    Much improved form validation for adding thermo entries to database.
    Clicking "Submit" on the form now causes a large amount of validation to
    proceed. If the form is deemed valid, an object containing the cleaned data
    is created. This object is ready for adding to the database.
    
    Key takeaway: Processing of the POST command in the appropriate view should
    use only the *cleaned* data, i.e. the data should already be in a form
    suitable for adding to the database at this point. This means that type and
    unit conversions should be done in other places.

--- website/database/forms.py
+++ website/database/forms.py
@@ -1,4 +1,9 @@
 from django import forms
+from django.forms.util import ErrorList
+from django.utils.safestring import mark_safe
+
+import chempy.constants as constants
+from chempy.molecule import Molecule
 
 ################################################################################
 
@@ -9,6 +14,12 @@ TEMPERATURE_UNITS = (
     ('R', u'\u00B0R'),
 )
 
+def convertTemperature(value, units):
+    if units == 'K':   return value
+    elif units == 'C': return value + 273.15
+    elif units == 'F': return (value + 459.67) / 1.8
+    elif units == 'R': return value / 1.8
+
 PRESSURE_UNITS = (
     ('bar', 'bar'),
     ('atm', 'atm'),
@@ -16,6 +27,13 @@ PRESSURE_UNITS = (
     ('psi', 'psi'),
     ('torr', 'torr'),
 )
+
+def convertPressure(value, units):
+    if units == 'bar':    return value * 1.0e5
+    elif units == 'atm':  return value * 101325.
+    elif units == 'Pa':   return value
+    elif units == 'psi':  return value * 6894.757
+    elif units == 'torr': return value * 101325. / 760.0
 
 ENERGY_UNITS = (
     ('kJ/mol', 'kJ/mol'),
@@ -26,24 +44,45 @@ ENERGY_UNITS = (
 
 )
 
+def convertEnergy(value, units):
+    if units == 'kJ/mol':     return value * 1000.
+    elif units == 'kcal/mol': return value * 4184.
+    elif units == 'J/mol':    return value
+    elif units == 'cal/mol':  return value * 4.184
+    elif units == 'cm^-1':    return value * constants.c * 100. * constants.h * constants.Na
+
 HEAT_CAPACITY_UNITS = (
     ('J/mol*K', 'J/mol*K'),
     ('cal/mol*K', 'cal/mol*K'),
 )
 
+def convertHeatCapacity(value, units):
+    if units == 'J/mol*K':     return value
+    elif units == 'cal/mol*K': return value * 4.184
+
 REFERENCE_TYPES = [
+    ('',''),
     ('theoretical', 'Theoretical'),
     ('experimental', 'Experimental'),
     ('review', 'Review'),
 ]
 
 THERMODATA_TYPES = [
+    ('',''),
     ('group additivity', 'Group additivity'),
     ('wilhoit', 'Wilhoit polynomial'),
     ('nasa', 'NASA polynomial'),
 ]
 
 ################################################################################
+
+class DivErrorList(ErrorList):
+    def __unicode__(self):
+        return self.as_divs()
+    def as_divs(self):
+        if not self: return u''
+        return mark_safe(u'<label>&nbsp;</label>%s' % (''.join([u'<div class="error">%s</div>' % e for e in self])))
+        #return u'<div class="errorlist">%s</div>' % ''.join([u'<div class="error">%s</div>' % e for e in self])
 
 class DataEntryForm(forms.Form):
     """
@@ -72,6 +111,45 @@ class ThermoEntryForm(DataEntryForm):
     Tmax = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
     Tmax_units = forms.ChoiceField(choices=TEMPERATURE_UNITS)
     
+    def clean(self):
+        """
+        Custom form validation that requires access to multiple fields.
+        """
+        if self.data['dataFormat'] in ['group additivity', 'wilhoit']:
+            # Make sure minimum temperature is below maximum temperature
+            if 'Tmin' in self.cleaned_data and 'Tmax' in self.cleaned_data and self.cleaned_data['Tmin'] > self.cleaned_data['Tmax']:
+                self._errors['Tmin'] = self.error_class([u'The minimum temperature is greater than the maximum temperature.'])
+            
+        return DataEntryForm.clean(self)
+    
+    def clean_species(self):
+        """
+        Custom validation for the species field to ensure that a valid adjacency 
+        list has been provided.
+        """
+        try:
+            molecule = Molecule()
+            molecule.fromAdjacencyList(str(self.cleaned_data['species']))
+        except Exception, e:
+            import traceback
+            traceback.print_exc(e)
+            raise forms.ValidationError('Invalid adjacency list.')
+        return str(self.cleaned_data['species'])
+    
+    def clean_Tmin(self):
+        """
+        Custom processing for the minimum temperature field; converts the value
+        to have units of K.
+        """
+        return convertTemperature(self.cleaned_data['Tmin'], self.data['Tmin_units'])
+    
+    def clean_Tmax(self):
+        """
+        Custom processing for the maximum temperature field; converts the value
+        to have units of K.
+        """
+        return convertTemperature(self.cleaned_data['Tmax'], self.data['Tmax_units'])
+        
 class ThermoDataForm(forms.Form):
     
     Tlist = forms.CharField(widget=forms.widgets.Textarea(attrs={'rows':8, 'cols':10}))
@@ -83,6 +161,45 @@ class ThermoDataForm(forms.Form):
     H298_units = forms.ChoiceField(choices=ENERGY_UNITS)
     S298 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
     S298_units = forms.ChoiceField(choices=HEAT_CAPACITY_UNITS)
+    
+    def clean(self):
+        """
+        Custom form validation that requires access to multiple fields.
+        """
+        # Make sure we've given the same number of temperature points as heat capacity points
+        if 'Tlist' in self.cleaned_data and 'Cplist' in self.cleaned_data and len(self.cleaned_data['Tlist']) != len(self.cleaned_data['Cplist']):
+            self._errors['Cplist'] = self.error_class([u'Number of temperatures does not match number of heat capacities.'])
+        return self.cleaned_data
+    
+    def clean_Tlist(self):
+        """
+        Manual processing and validation of the Tlist field. Converts to a list of
+        floats, assuming the field contains one value per line.
+        """
+        try:
+            Tlist = [float(T.strip()) for T in self.cleaned_data['Tlist'].split()]
+            Tlist = [convertTemperature(T, self.data['T_units']) for T in Tlist]
+        except ValueError:
+            raise forms.ValidationError('One or more invalid entries in temperature list.')
+        return Tlist
+        
+    def clean_Cplist(self):
+        """
+        Manual processing and validation of the Cplist field. Converts to a list of
+        floats, assuming the field contains one value per line.
+        """
+        try:
+            Cplist = [float(T.strip()) for T in self.cleaned_data['Cplist'].split()]
+            Cplist = [convertHeatCapacity(Cp, self.data['Cp_units']) for Cp in Cplist]
+        except ValueError:
+            raise forms.ValidationError('One or more invalid entries in heat capacity list.')
+        return Cplist
+   
+    def clean_H298(self):
+        return convertEnergy(self.cleaned_data['H298'], self.data['H298_units'])
+    
+    def clean_S298(self):
+        return convertHeatCapacity(self.cleaned_data['S298'], self.data['S298_units'])
     
 class WilhoitForm(forms.Form):
     
@@ -104,12 +221,27 @@ class WilhoitForm(forms.Form):
     S0 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
     S0_units = forms.ChoiceField(choices=HEAT_CAPACITY_UNITS)
 
+    def clean_Cp0(self):
+        return convertHeatCapacity(self.cleaned_data['Cp0'], self.data['Cp0_units'])
+    
+    def clean_CpInf(self):
+        return convertHeatCapacity(self.cleaned_data['CpInf'], self.data['CpInf_units'])
+    
+    def clean_H0(self):
+        return convertEnergy(self.cleaned_data['H0'], self.data['H0_units'])
+    
+    def clean_S0(self):
+        return convertHeatCapacity(self.cleaned_data['S0'], self.data['S0_units'])
+    
+    def clean_B(self):
+        return convertTemperature(self.cleaned_data['B'], self.data['B_units'])
+
 class NASAForm(forms.Form):
     
     nasa_numPolys = forms.ChoiceField(choices=[('1','1'), ('2','2'), ('3','3')])
 
-    nasa_am2_0 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
-    nasa_am1_0 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_am2_0 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_am1_0 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
     nasa_a0_0 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
     nasa_a1_0 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
     nasa_a2_0 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
@@ -120,30 +252,87 @@ class NASAForm(forms.Form):
     nasa_Tmin_0 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
     nasa_Tmax_0 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
     
-    nasa_am2_1 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
-    nasa_am1_1 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
-    nasa_a0_1 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
-    nasa_a1_1 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
-    nasa_a2_1 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
-    nasa_a3_1 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
-    nasa_a4_1 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
-    nasa_a5_1 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
-    nasa_a6_1 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
-    nasa_Tmin_1 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
-    nasa_Tmax_1 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_am2_1 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_am1_1 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_a0_1 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_a1_1 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_a2_1 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_a3_1 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_a4_1 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_a5_1 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_a6_1 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_Tmin_1 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_Tmax_1 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
     
-    nasa_am2_2 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
-    nasa_am1_2 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
-    nasa_a0_2 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
-    nasa_a1_2 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
-    nasa_a2_2 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
-    nasa_a3_2 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
-    nasa_a4_2 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
-    nasa_a5_2 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
-    nasa_a6_2 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
-    nasa_Tmin_2 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
-    nasa_Tmax_2 = forms.FloatField(widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_am2_2 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_am1_2 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_a0_2 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_a1_2 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_a2_2 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_a3_2 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_a4_2 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_a5_2 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_a6_2 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_Tmin_2 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
+    nasa_Tmax_2 = forms.FloatField(required=False, widget=forms.widgets.Input(attrs={'class': 'numberInput'}))
     
-    nasa_Tmin_units = forms.ChoiceField(choices=TEMPERATURE_UNITS)
-    nasa_Tmax_units = forms.ChoiceField(choices=TEMPERATURE_UNITS)
+    nasa_Tmin_units = forms.ChoiceField(required=False, choices=TEMPERATURE_UNITS)
+    nasa_Tmax_units = forms.ChoiceField(required=False, choices=TEMPERATURE_UNITS)
     
+    def clean(self):
+        """
+        Custom form validation that requires access to multiple fields.
+        """
+        numPolys = self.cleaned_data['nasa_numPolys']
+        if numPolys > 2:
+            for field in ['nasa_a0_2', 'nasa_a1_2', 'nasa_a2_2', 'nasa_a3_2', 'nasa_a4_2', 'nasa_a5_2', 'nasa_a6_2', 'nasa_Tmin_2', 'nasa_Tmax_2']:
+                if self.cleaned_data[field] is None:
+                    self._errors[field] = self.error_class([u'This field is required.'])
+        if numPolys > 1:
+            for field in ['nasa_a0_1', 'nasa_a1_1', 'nasa_a2_1', 'nasa_a3_1', 'nasa_a4_1', 'nasa_a5_1', 'nasa_a6_1', 'nasa_Tmin_1', 'nasa_Tmax_1']:
+                if self.cleaned_data[field] is None:
+                    self._errors[field] = self.error_class([u'This field is required.'])
+        if numPolys > 0:
+            for field in ['nasa_a0_0', 'nasa_a1_0', 'nasa_a2_0', 'nasa_a3_0', 'nasa_a4_0', 'nasa_a5_0', 'nasa_a6_0', 'nasa_Tmin_0', 'nasa_Tmax_0']:
+                if self.cleaned_data[field] is None:
+                    self._errors[field] = self.error_class([u'This field is required.'])
+        return self.cleaned_data
+    
+    def clean_nasa_numPolys(self):
+        return int(self.data['nasa_numPolys'])
+    
+    def clean_nasa_am2_0(self):
+        return self.cleaned_data['nasa_am2_0'] if self.cleaned_data['nasa_am2_0'] else 0.
+
+    def clean_nasa_am1_0(self):
+        return self.cleaned_data['nasa_am1_0'] if self.cleaned_data['nasa_am1_0'] else 0.
+    
+    def clean_nasa_am2_1(self):
+        return self.cleaned_data['nasa_am2_1'] if self.cleaned_data['nasa_am2_1'] else 0.
+    
+    def clean_nasa_am1_1(self):
+        return self.cleaned_data['nasa_am1_1'] if self.cleaned_data['nasa_am1_1'] else 0.
+    
+    def clean_nasa_am2_2(self):
+        return self.cleaned_data['nasa_am2_2'] if self.cleaned_data['nasa_am2_2'] else 0.
+    
+    def clean_nasa_am1_2(self):
+        return self.cleaned_data['nasa_am1_2'] if self.cleaned_data['nasa_am1_2'] else 0.
+    
+    def clean_nasa_Tmin_0(self):
+        return convertTemperature(self.cleaned_data['nasa_Tmin_0'], self.data['nasa_Tmin_units'])
+        
+    def clean_nasa_Tmax_0(self):
+        return convertTemperature(self.cleaned_data['nasa_Tmax_0'], self.data['nasa_Tmax_units'])
+        
+    def clean_nasa_Tmin_1(self):
+        return convertTemperature(self.cleaned_data['nasa_Tmin_1'], self.data['nasa_Tmin_units'])
+        
+    def clean_nasa_Tmax_1(self):
+        return convertTemperature(self.cleaned_data['nasa_Tmax_1'], self.data['nasa_Tmax_units'])
+        
+    def clean_nasa_Tmin_2(self):
+        return convertTemperature(self.cleaned_data['nasa_Tmin_2'], self.data['nasa_Tmin_units'])
+        
+    def clean_nasa_Tmax_2(self):
+        return convertTemperature(self.cleaned_data['nasa_Tmax_2'], self.data['nasa_Tmax_units'])
--- website/database/views.py
+++ website/database/views.py
@@ -1,5 +1,10 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 from forms import *
+
+from chempy.thermo import ThermoGAModel, WilhoitModel, NASAPolynomial, NASAModel
+from rmgdata.thermo import ThermoEntry
 
 def index(request):
     """
@@ -11,12 +16,94 @@ def addThermoEntry(request):
     """
     The homepage for RMG database viewing and editing.
     """
-    form = ThermoEntryForm()
-    thermoDataForm = ThermoDataForm()
-    wilhoitForm = WilhoitForm()
-    nasaForm = NASAForm()
+    if request.method == 'POST':
+        form = ThermoEntryForm(request.POST, error_class=DivErrorList)
+        thermoDataForm = ThermoDataForm(request.POST, error_class=DivErrorList)
+        wilhoitForm = WilhoitForm(request.POST, error_class=DivErrorList)
+        nasaForm = NASAForm(request.POST, error_class=DivErrorList)
+        
+        thermoData = None
+        
+        if form.is_valid():
+            molecule = Molecule()
+            molecule.fromAdjacencyList(form.cleaned_data['species'])
+            Tmin = form.cleaned_data['Tmin']
+            Tmax = form.cleaned_data['Tmax']
+            reference = form.cleaned_data['reference']
+            referenceLink = form.cleaned_data['referenceLink']
+            referenceType = form.cleaned_data['referenceType']
+            shortDesc = form.cleaned_data['shortDesc']
+            longDesc = form.cleaned_data['longDesc']
+            
+            if form.data['dataFormat'] == 'group additivity' and thermoDataForm.is_valid():
+                thermoData = ThermoGAModel(
+                    Tdata = thermoDataForm.cleaned_data['Tlist'], 
+                    Cpdata = thermoDataForm.cleaned_data['Cplist'], 
+                    H298 = thermoDataForm.cleaned_data['H298'], 
+                    S298 = thermoDataForm.cleaned_data['S298'], 
+                    Tmin = Tmin, 
+                    Tmax = Tmax,
+                )
+                    
+            elif form.data['dataFormat'] == 'wilhoit' and wilhoitForm.is_valid():
+                thermoData = WilhoitModel(
+                    cp0 = wilhoitForm.cleaned_data['Cp0'], 
+                    cpInf = wilhoitForm.cleaned_data['CpInf'], 
+                    a0 = wilhoitForm.cleaned_data['a0'], 
+                    a1 = wilhoitForm.cleaned_data['a1'], 
+                    a2 = wilhoitForm.cleaned_data['a2'], 
+                    a3 = wilhoitForm.cleaned_data['a3'], 
+                    H0 = wilhoitForm.cleaned_data['H0'], 
+                    S0 = wilhoitForm.cleaned_data['S0'], 
+                    B = wilhoitForm.cleaned_data['B'],
+                )
+                thermoData.Tmin = Tmin
+                thermoData.Tmax = Tmax
+                
+            elif form.data['dataFormat'] == 'nasa' and nasaForm.is_valid():
+                numPolys = int(nasaForm.cleaned_data['nasa_numPolys'])
+                thermoData = NASAModel(Tmin=Tmin, Tmax=Tmax)
+                for i in range(3):
+                    if i < numPolys:
+                        poly = NASAPolynomial(
+                            coeffs = [
+                                nasaForm.cleaned_data['nasa_am2_%i' % i], 
+                                nasaForm.cleaned_data['nasa_am1_%i' % i], 
+                                nasaForm.cleaned_data['nasa_a0_%i' % i], 
+                                nasaForm.cleaned_data['nasa_a1_%i' % i], 
+                                nasaForm.cleaned_data['nasa_a2_%i' % i], 
+                                nasaForm.cleaned_data['nasa_a3_%i' % i], 
+                                nasaForm.cleaned_data['nasa_a4_%i' % i], 
+                                nasaForm.cleaned_data['nasa_a5_%i' % i], 
+                                nasaForm.cleaned_data['nasa_a6_%i' % i], 
+                            ],
+                            Tmin=nasaForm.cleaned_data['nasa_Tmin_%i' % i], 
+                            Tmax=nasaForm.cleaned_data['nasa_Tmax_%i' % i],
+                        )
+                        thermoData.polynomials.append(poly)
+        
+            if thermoData is not None:
+                thermoEntry = ThermoEntry(
+                    data=thermoData, 
+                    reference=reference, 
+                    referenceLink=referenceLink, 
+                    referenceType=referenceType, 
+                    shortDesc=shortDesc, 
+                    longDesc=longDesc,
+                )
+                print repr(thermoData)
+                
+                print 'Form is valid!!!!'
+        #       return HttpResponseRedirect('/database/')
+    else:
+        form = ThermoEntryForm(error_class=DivErrorList)
+        thermoDataForm = ThermoDataForm(error_class=DivErrorList)
+        wilhoitForm = WilhoitForm(error_class=DivErrorList)
+        nasaForm = NASAForm(error_class=DivErrorList)
+    
     return render_to_response('addThermoEntry.html', 
-        {'form': form, 'thermoDataForm': thermoDataForm, 'wilhoitForm': wilhoitForm, 'nasaForm': nasaForm})
+        {'form': form, 'thermoDataForm': thermoDataForm, 'wilhoitForm': wilhoitForm, 'nasaForm': nasaForm},
+        context_instance=RequestContext(request))
 
 def addKineticsEntry(request):
     """
