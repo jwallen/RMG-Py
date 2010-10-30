e908e1445a467292e1602093ef976096af59baa8
Josh Allen <jwallen@mit.edu>
2010-10-30 14:43:01 -0400

    Thermo entry viewer now works for GA, Wilhoit, and NASA thermo models.
    The template can handle any number of Cp(T) points supplied to the
    ThermoGAModel and any number of NASA polynomials. This is also true of
    the thermo evaluator tool. The parameters for each model are now
    displayed using a two-column table.
    
    The Javascript code for the thermo evaluator has been moved from the
    viewThermoEntry template to the thermoEvalForm template. This way we can
    potentially use this template in other places as well.

--- website/database/views.py
+++ website/database/views.py
@@ -127,7 +127,9 @@ def viewThermoEntry(request, index):
     entry = ThermoEntry(
         index=1,
         molecule="""methane\n1 C 0""",
-        data=WilhoitModel(1, 2, 3, 4, 5, 6, 7000, 8, 9, 1000), 
+        #data=WilhoitModel(1, 2, 3, 4, 5, 6, 7000, 8, 9, 1000), 
+        #data=ThermoGAModel(Tdata=[300,400,500,600,800,1000,1500], Cpdata=[1, 2, 3, 4, 5, 6, 7], H298=1000, S298=20), 
+        data=NASAModel(polynomials=[NASAPolynomial(Tmin=300, Tmax=1000, coeffs=[1, 2, 3, 4, 5, 6, 7]), NASAPolynomial(Tmin=1000, Tmax=2000, coeffs=[1, 2, 3, 4, 5, 6, 7, 8, 9])]), 
         reference="""n/a""", 
         referenceLink='n/a', 
         referenceType='n/a', 
@@ -141,20 +143,29 @@ def viewThermoEntry(request, index):
     )
     form = ThermoEvalForm()
     entry.data.Tmin = 300; entry.data.Tmax = 2000
-    if isinstance(entry.data, WilhoitModel):
-        parameters = {'form': form, 'entry': entry, 'type': 'wilhoit',
-            'Cp0': entry.data.cp0,
-            'CpInf': entry.data.cpInf,
-            'a0': entry.data.a0,
-            'a1': entry.data.a1,
-            'a2': entry.data.a2,
-            'a3': entry.data.a3,
-            'B': entry.data.B,
-            'H0': entry.data.H0 / 1000.,
-            'S0': entry.data.S0,
-            'Tmin': entry.data.Tmin,
-            'Tmax': entry.data.Tmax,
-        }
+    parameters = {'form': form, 'entry': entry, 'type': ''}
+    if isinstance(entry.data, ThermoGAModel):
+        parameters['format'] = 'Group additivity'
+        parameters['Cpdata'] = zip(entry.data.Tdata, entry.data.Cpdata)
+        parameters['H298'] = entry.data.H298 / 1000.
+        parameters['S298'] = entry.data.S298
+    elif isinstance(entry.data, WilhoitModel):
+        parameters['format'] = 'Wilhoit polynomial'
+        parameters['Cp0'] = entry.data.cp0
+        parameters['CpInf'] = entry.data.cpInf
+        parameters['a0'] = entry.data.a0
+        parameters['a1'] = entry.data.a1
+        parameters['a2'] = entry.data.a2
+        parameters['a3'] = entry.data.a3
+        parameters['B'] = entry.data.B
+        parameters['H0'] = entry.data.H0 / 1000.
+        parameters['S0'] = entry.data.S0
+    elif isinstance(entry.data, NASAModel):
+        parameters['format'] = 'NASA polynomials'
+        parameters['polynomials'] = entry.data.polynomials
+        
+    parameters['Tmin'] = entry.data.Tmin
+    parameters['Tmax'] = entry.data.Tmax
     return render_to_response('viewThermoEntry.html', parameters, context_instance=RequestContext(request))
 
 def addKineticsEntry(request):
--- website/media/chem.js
+++ website/media/chem.js
@@ -52,6 +52,57 @@ function convertHeatCapacityTo(value, units) {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+function thermoGA_getHeatCapacity(T, Tdata, Cpdata, H298, S298) {
+    Cp = 0.0;
+    if (T < Tdata[0])
+        Cp = Cpdata[0];
+    else if (T >= Tdata[Tdata.length-1])
+        Cp = Cpdata[Cpdata.length-1];
+    else { 
+        for (var i = 0; i < Tdata.length-1; i++) {
+            var Tmin  = Tdata [i]; var Tmax  = Tdata [i+1];
+            var Cpmin = Cpdata[i]; var Cpmax = Cpdata[i+1];
+            if (Tmin <= T && T < Tmax)
+                Cp = (Cpmax - Cpmin) * ((T - Tmin) / (Tmax - Tmin)) + Cpmin;
+        }
+    }
+    return Cp;
+}
+
+function thermoGA_getEnthalpy(T, Tdata, Cpdata, H298, S298) {
+    H = H298;
+    for (var i = 0; i < Tdata.length-1; i++) {
+        var Tmin  = Tdata [i]; var Tmax  = Tdata [i+1];
+        var Cpmin = Cpdata[i]; var Cpmax = Cpdata[i+1];
+        if (T > Tmin) {
+            var slope = (Cpmax - Cpmin) / (Tmax - Tmin);
+            var intercept = (Cpmin * Tmax - Cpmax * Tmin) / (Tmax - Tmin);
+            if (T < Tmax) H += 0.5 * slope * (T*T - Tmin*Tmin) + intercept * (T - Tmin);
+            else          H += 0.5 * slope * (Tmax*Tmax - Tmin*Tmin) + intercept * (Tmax - Tmin);
+        }
+    }
+    if (T > Tdata[Tdata.length-1])
+        H += Cpdata[Cpdata.length-1] * (T - Tdata[Tdata.length-1]);
+    return H;
+}
+
+function thermoGA_getEntropy(T, Tdata, Cpdata, H298, S298) {
+    S = S298;
+    for (var i = 0; i < Tdata.length-1; i++) {
+        var Tmin  = Tdata [i]; var Tmax  = Tdata [i+1];
+        var Cpmin = Cpdata[i]; var Cpmax = Cpdata[i+1];
+        if (T > Tmin) {
+            var slope = (Cpmax - Cpmin) / (Tmax - Tmin);
+            var intercept = (Cpmin * Tmax - Cpmax * Tmin) / (Tmax - Tmin);
+            if (T < Tmax) S += slope * (T - Tmin) + intercept * Math.log(T/Tmin);
+            else          S += slope * (Tmax - Tmin) + intercept * Math.log(Tmax/Tmin);
+        }
+    }
+    if (T > Tdata[Tdata.length-1])
+        S += Cpdata[Cpdata.length-1] * Math.log(T / Tdata[Tdata.length-1]);
+    return S;
+}
+
 function wilhoit_getHeatCapacity(T, Cp0, CpInf, a0, a1, a2, a3, B, H0, S0) {
     var y = T / (T + B);
     return Cp0 + (CpInf - Cp0)*y*y*(1 + (y-1)*(a0 + y*(a1 + y*(a2 + y*a3))));
@@ -69,4 +120,20 @@ function wilhoit_getEntropy(T, Cp0, CpInf, a0, a1, a2, a3, B, H0, S0) {
     var logt = Math.log(T);
     var logy = Math.log(y);
     return S0 + CpInf*logt-(CpInf-Cp0)*(logy+y*(1+y*(a0/2+y*(a1/3 + y*(a2/4 + y*a3/5)))));
+}
+
+function nasaPolynomial_getHeatCapacity(T, coeffs) {
+    return ((coeffs[0] / T + coeffs[1]) / T + coeffs[2] + T*(coeffs[3] + T*(coeffs[4] + T*(coeffs[5] + coeffs[6]*T)))) * 8.314472;
+}
+
+function nasaPolynomial_getEnthalpy(T, coeffs) {
+    var T2 = T*T;
+    var T4 = T2*T2;
+    return ((-coeffs[0] / T + coeffs[1] * Math.log(T)) / T + coeffs[2] + coeffs[3]*T/2. + coeffs[4]*T2/3. + coeffs[5]*T2*T/4. + coeffs[6]*T4/5. + coeffs[7]/T) * 8.314472 * T;
+}
+
+function nasaPolynomial_getEntropy(T, coeffs) {
+    var T2 = T*T;
+    var T4 = T2*T2;
+    return ((-coeffs[0] / T / 2. - coeffs[1]) / T + coeffs[2]*Math.log(T) + coeffs[3]*T + coeffs[4]*T2/2. + coeffs[5]*T2*T/3. + coeffs[6]*T4/4. + coeffs[8] ) * 8.314472;
 }
